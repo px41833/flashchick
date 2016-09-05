a712a92b4357872f0c1900efdb4b0d33566b814f
pangxin <1138471665@qq.com>
2016-09-05 16:39:09 +0800

    二次提交

---
+++ ybk2/ybk2/ybk2.h
@@ -0,0 +1,32 @@
+
+// ybk2.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// Cybk2App:
+// 有关此类的实现，请参阅 ybk2.cpp
+//
+
+class Cybk2App : public CWinApp
+{
+public:
+	Cybk2App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Cybk2App theApp;
---
+++ ybk2/ybk2/ybk2Dlg.h
@@ -0,0 +1,36 @@
+
+// ybk2Dlg.h : 头文件
+//
+
+#pragma once
+#include "afxwin.h"
+
+
+// Cybk2Dlg 对话框
+class Cybk2Dlg : public CDialogEx
+{
+// 构造
+public:
+	Cybk2Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_YBK2_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnCbnSelchangeCombo1();
+	CComboBox m_editAddend;
+};
