f09865849c5c35230fb2923d70064c3d02882b8e
mimi <mimi41833@126.com>
2016-09-12 23:31:41 +0800

    555

--- test/test.cpp
+++ test/test.cpp
@@ -8,6 +8,20 @@
 #define MYPORT  16915
 #define IPADDR  "123.59.182.105"
 #define BUFFER_SIZE 1024
+time_t GetCurrMSForMe() //获取1970年到现在的毫秒数
+{
+	SYSTEMTIME tTime = {0};
+	GetSystemTime(&tTime);
+
+	FILETIME fTime = {0};
+	SystemTimeToFileTime(&tTime, &fTime);
+
+	ULARGE_INTEGER ui;
+	ui.LowPart = fTime.dwLowDateTime;
+	ui.HighPart = fTime.dwHighDateTime;
+
+	return ((LONGLONG)(ui.QuadPart-116444736000000000)/10000);
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
 		WORD sockVersion = MAKEWORD(2,2);
@@ -61,7 +75,7 @@ int _tmain(int argc, _TCHAR* argv[])
 			
 			strcat(send_str, "<?xml version=\"1.0\" encoding=\"gb2312\"?><GNNT><REQ name=\"logon\"><USER_ID>1299906727</USER_ID><PASSWORD>418331101</PASSWORD><REGISTER_WORD></REGISTER_WORD><VERSIONINFO></VERSIONINFO><LOGONTYPE>pc</LOGONTYPE></REQ></GNNT>");
 
-			printf("!!!!!!!!!!444!!!!!!!!!!!!!send_str:%s\n",send_str);
+			printf("!!!!!!!!!!11step!!!!!!!!!!!!!send_str:%s\n",send_str);
 		send(sclient, send_str, strlen(send_str), 0);
 
 		 recv(sclient, recvbuf, sizeof(recvbuf),0); ///接收
@@ -89,7 +103,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		 strcat(send_str,retcode);
 		 strcat(send_str,"</SESSION_ID></REQ></GNNT>");
 
-		 printf("!!!!!!!!!!77777!!!!!!!!!!!!!send_str:%s\n",send_str);
+		 printf("!!!!!!!!!!22step!!!!!!!!!!!!!send_str:%s\n",send_str);
 
 		 send(sclient, send_str, strlen(send_str),0); ///发送
 		 //if(strcmp(sendbuf,"exit\n")==0)
@@ -97,6 +111,73 @@ int _tmain(int argc, _TCHAR* argv[])
 		 recv(sclient, recvbuf, sizeof(recvbuf),0); ///接收
 		 fputs(recvbuf, stdout);
 
+		 memset(sendbuf, 0, sizeof(sendbuf));
+		 memset(recvbuf, 0, sizeof(recvbuf));
+		 memset(send_str, 0, sizeof(send_str));
+
+		 //头信息
+		 strcat(send_str, "POST ");
+		 strcat(send_str, api);
+		 strcat(send_str, " HTTP/1.1\r\n");
+		 strcat(send_str, "Host: ");
+		 strcat(send_str, hostname);
+		 strcat(send_str, "\r\n");
+		 strcat(send_str, "Accept-Encoding:identity\r\n");
+		 strcat(send_str, "Content-Length:188\r\n\r\n");
+
+		 strcat(send_str, "<?xml version=\"1.0\" encoding=\"gb2312\"?><GNNT><REQ name=\"commodity_query\"><USER_ID>1299906727</USER_ID><COMMODITY_ID></COMMODITY_ID><SESSION_ID>");
+		 strcat(send_str,retcode);
+		 strcat(send_str,"</SESSION_ID></REQ></GNNT>");
+
+		 printf("!!!!!!!!!!3 step!!!!!!!!!!!!!send_str:%s\n",send_str);
+
+		 /*send(sclient, send_str, strlen(send_str),0); ///发送
+
+		 while(0!=recv(sclient, recvbuf, sizeof(recvbuf),0)) ///接收
+		 fputs(recvbuf, stdout);*/
+
+		 memset(sendbuf, 0, sizeof(sendbuf));
+		 memset(recvbuf, 0, sizeof(recvbuf));
+		 memset(send_str, 0, sizeof(send_str));
+
+		 //头信息
+		 strcat(send_str, "POST ");
+		 strcat(send_str, api);
+		 strcat(send_str, " HTTP/1.1\r\n");
+		 strcat(send_str, "Host: ");
+		 strcat(send_str, hostname);
+		 strcat(send_str, "\r\n");
+		 strcat(send_str, "Accept-Encoding:identity\r\n");
+		 strcat(send_str, "Content-Length:212\r\n\r\n");
+
+		 strcat(send_str, "<?xml version=\"1.0\" encoding=\"gb2312\"?><GNNT><REQ name=\"sys_time_query\"><USER_ID>1299906727</USER_ID><LAST_ID>0</LAST_ID><TD_CNT>0</TD_CNT><SESSION_ID>");
+		 strcat(send_str,retcode);
+		 strcat(send_str,"</SESSION_ID><CU_LG>1</CU_LG></REQ></GNNT>");
+
+		 printf("!!!!!!!!!!4 step syn time!!!!!!!!!!!!!send_str:%s\n",send_str);
+		 SYSTEMTIME st;
+
+		// CString strDate,strTime;
+		 for (int i=0;i<4;i++)
+		 {
+	
+		 send(sclient, send_str, strlen(send_str),0); ///发送
+
+		 recv(sclient, recvbuf, sizeof(recvbuf),0); ///接收
+		 fputs(recvbuf, stdout);
+		 
+
+		 printf("\r\ntime:%lld\n",GetCurrMSForMe());
+		 //GetLocalTime(&st);
+		 //printf("\r\ni:%d,time:%u:%u:%u:%d:%d:%d:%d\n",i,st.wYear,st.wMonth,st.wDay,st.wHour,st.wMinute,st.wSecond,st.wMilliseconds);
+		// strDate.Format("M----",st.wYear,st.wMonth,st.wDay);
+
+		 //strTime.Format("-:-:-",st.wHour,st.wMinute,st.wSecond) ;
+
+		 //AfxMessageBox(strDate);
+
+		 //AfxMessageBox(strTime);
+		 }
 		closesocket(sclient);
 		WSACleanup();
 
---
+++ ybk2/ybk2/YBClientNet.h
@@ -0,0 +1,8 @@
+#pragma once
+class YBClientNet
+{
+public:
+	YBClientNet(void);
+	~YBClientNet(void);
+};
+
